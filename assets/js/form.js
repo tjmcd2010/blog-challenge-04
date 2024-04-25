02d8c8bd8cbb7e4edb1d371495b77c23a8312617
Travis McDermott <tjmcd2010@gmail.com>
2024-04-24 22:10:19 -0700

    updates all forms and readme

--- assets/js/form.js
+++ assets/js/form.js
@@ -13,25 +13,7 @@ function auth(event) {
         alert("Please enter all the fields");
     } 
 }
-// collect the data from the form in local storage and display it on blog.html
-function collectData() { 
-    let username = document.getElementById("username").value;
-    let title = document.getElementById("title").value;
-    let blog = document.getElementById("blog").value;
-
-    localStorage.setItem("username", username);
-    localStorage.setItem("title", title);
-    localStorage.setItem("blog", blog);
-} 
-//place data in local storage
-
-function getFormData() {
-    const username = localStorage.setItem("username");
-    const title = localStorage.setItem("title");
-    const blog = localStorage.setItem("blog");
-    document.getElementById("username").innerHTML = username;
-    document.getElementById("title").innerHTML = title;
-    document.getElementById("blog").innerHTML = blog;
-} 
+//WHEN I submit the form, THEN the page is redirected to blog.html
+document.getElementById("form").addEventListener("submit", auth);
 
 
